67dc805209dfd66d16c761b3cabad7d57c1b79d2
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2020-12-21 13:59:22 +0100

    timer working

---
+++ Assets/MyGame/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timer;
+    private float timeInSec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeInSec = 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float minutes = Mathf.FloorToInt(timeInSec/60);
+        float seconds =Mathf.FloorToInt(timeInSec%60);
+        timeInSec -= Time.deltaTime;
+
+        if(timeInSec > 0)
+        {
+            //timeInSec = timer;
+            timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        }
+        else
+        {
+            Debug.Log("stop");
+        }
+    }
+}
