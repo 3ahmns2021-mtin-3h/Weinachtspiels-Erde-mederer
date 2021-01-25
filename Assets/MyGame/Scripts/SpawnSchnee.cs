57f49aba8df57c325b9ab4818e3cb8727732fa2f
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2021-01-25 13:21:51 +0100

    Schnee und Wasser Fallen zufällig hintereinander

---
+++ Assets/MyGame/Scripts/SpawnSchnee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSchnee : MonoBehaviour
+{
+    public GameObject schneePrefab;
+    public GameObject parentSpawnGo;
+
+    private float spawnRate = 3f;
+    private float nextSpawn = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTheSnow();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            SpawnTheSnow();
+            
+        }
+    }
+    private void SpawnTheSnow()
+    {
+        var clones = Instantiate(schneePrefab);
+        clones.GetComponent<Transform>().SetParent(parentSpawnGo.GetComponent<Transform>(), false);
+        clones.GetComponent<Transform>().localPosition = new Vector3(Random.Range(-400, 400), 0, 0) ;
+        clones.GetComponent<Transform>().localRotation = Quaternion.identity;
+        clones.GetComponent<Transform>().localScale = new Vector3 (1f, 1f, 1f);
+    }
+}
